ee4e0e01bda732eb1348e5d6fe0942fee74bc9e5
Welinton Suarez <wesgdeveloper@gmail.com>
2023-11-06 14:13:49 -0500

    add CRUD for routes Blogs

---
+++ app/api/v1/blogs/model.js
@@ -0,0 +1,8 @@
+export const fields = [
+  "id",
+  "title",
+  "content",
+  "createdAt",
+  "updatedAt",
+  "url_foto",
+];
--- app/config.js
+++ app/config.js
@@ -6,4 +6,14 @@ export const configuration = {
   server: {
     port: process.env.PORT,
   },
+
+  pagination: {
+    limit: 10,
+    offset: 0,
+  },
+  order: {
+    options: ["asc", "desc"],
+    direction: "desc",
+    orderBy: "createdAt",
+  },
 };
