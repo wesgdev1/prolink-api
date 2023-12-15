41b1014caf79c984a08f079ca9adf76431c8c537
Welinton Suarez <wesgdeveloper@gmail.com>
2023-12-14 21:40:32 -0500

    fix: se retorna telefono cliente en controlador MisSoportes

--- app/api/v1/soportes/controller.js
+++ app/api/v1/soportes/controller.js
@@ -78,6 +78,7 @@ export const getMySoportes = async (req, res, next) => {
               nombreCompleto: true,
               email: true,
               direccion: true,
+              telefono: true,
             },
           },
         },
